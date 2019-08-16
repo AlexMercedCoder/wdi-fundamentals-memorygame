d4d52505871a09e00816a62e099b31bcdb2a1f9a
Alex Merced <WillCodeforFood@AlexMerced.com>
2019-08-16 14:50:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,38 @@
 console.log("Up and running!");
 
-var cards = ["King","Queen","King","Queen"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+},
+
+];
 
 var cardsInPlay = [];
 
 function flipCard (cardId)
 {
 
-	console.log ("User flipped a " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log ("User flipped a " + cards[cardId].rank);
+	console.log ("Suit " + cards[cardId].suit);
+	console.log ("Image " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 
 	
