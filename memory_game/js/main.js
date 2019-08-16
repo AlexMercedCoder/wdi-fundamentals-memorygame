ed250335060736e76372ce8aebe326e1d8ea903a
Alex Merced <WillCodeforFood@AlexMerced.com>
2019-08-16 12:18:18 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
 console.log("Up and running!");
 
-var cardOne = "King";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "Queen";
+var cards = ["King","Queen","King","Queen"];
 
-console.log("User Flipped "+ cardOne);
-console.log("User Flipped "+ cardTwo);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log ("User FLipped " + cardsInPlay[0]);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log ("User FLipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+
+	if (cardOne === cardTwo)
+		{console.log ("You got a match, you win!");}
+	else
+		{console.log ("Please try again!");}
+};
